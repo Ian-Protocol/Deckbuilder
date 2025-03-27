5a8e9132a674dadbe5494c3ea73a54514ce495c7
Ian-Protocol <94944192+Ian-Protocol@users.noreply.github.com>
2025-03-26 19:48:22 -0500

    Add save_deck_cards and image processing.

--- create_deck.php
+++ create_deck.php
@@ -6,6 +6,11 @@ require './includes/authenticate.php';
 // Connects to the database.
 require './includes/connect.php';
 
+// Image resizing if necessary.
+// require 'C:\xampp\htdocs\_Assignments\php-image-resize-master\lib\ImageResize.php';
+// require 'C:\xampp\htdocs\_Assignments\php-image-resize-master\lib\ImageResizeException.php';
+// use \Gumlet\ImageResize;
+
 $page_title = "Commander Deckbuilder - Create Deck";
 
 // TODO: Figure out users.
@@ -81,18 +86,19 @@ function parse_decklist($decklist_raw) {
 }
 
 // Saves the deck list in the decks table.
-function save_deck($db, $title, $description, $commander_id, $archetype, $user_id) {
+function save_deck($db, $title, $description, $commander_id, $image_path, $archetype, $user_id) {
         // Query & binding.
         //  Build the parameterized SQL query and bind to the sanitized values.
         // TODO: Create a dynamic version like in fetch_card.
-        $query = "INSERT INTO decks (user_id, title, description, archetype, card_id) 
-        VALUES (:user_id, :title, :description, :archetype, :card_id)";
+        $query = "INSERT INTO decks (user_id, title, description, image_path, archetype, card_id) 
+        VALUES (:user_id, :title, :description, :image_path, :archetype, :card_id)";
         $statement = $db -> prepare($query);
 
         //  Bind values to the parameters
         $statement -> bindValue(':user_id', $user_id);
         $statement -> bindValue(':title', $title);
         $statement -> bindValue(':description', $description);
+        $statement -> bindValue(':image_path', $image_path);
         $statement -> bindValue(':archetype', $archetype);
         $statement -> bindValue(':card_id', $commander_id);
 
@@ -109,15 +115,14 @@ function save_deck($db, $title, $description, $commander_id, $archetype, $user_i
 }
 
 function find_card($db, $card_name) {
-    $query = "SELECT id FROM cards WHERE name = :card_name";
+    $query = "SELECT card_id FROM cards WHERE name = :card_name";
     $statement = $db->prepare($query);
-
     $statement -> bindValue(':card_name', $card_name);
-
     $statement -> execute();
+    $result = $statement->fetch();
 
-    if ($statement) {
-        return $statement['id'];
+    if ($result) {
+        return $result['card_id'];
     } else {
         return fetch_card($db, $card_name);
     }
@@ -164,7 +169,6 @@ function fetch_card($db, $card_name) {
         }
 
         $statement -> execute();
-
         $card_id = $db -> lastInsertId();
 
         // Insert color identity into cards_mana_types table.
@@ -180,7 +184,7 @@ function fetch_card($db, $card_name) {
 function insert_card_colors($db, $card_id, $color_identity) {
     foreach ($color_identity as $color_code) {
         // Acquire mana type ID from database.
-        $select_id_query = "SELECT id FROM mana_types WHERE mana_type = :color_code";
+        $select_id_query = "SELECT mana_id FROM mana_types WHERE mana_type = :color_code";
 
         $statement = $db -> prepare($select_id_query);
         $statement -> bindValue(':color_code', $color_code);
@@ -188,13 +192,65 @@ function insert_card_colors($db, $card_id, $color_identity) {
         $result = $statement -> fetch();
         $mana_id = $result['mana_id'];
 
-        $insert_query = "INSERT OR IGNORE INTO cards_mana_types (card_id, mana_id)
+        $insert_query = "INSERT INTO cards_mana_types (card_id, mana_id)
         VALUES (:card_id, :mana_id)";
 
         $statement = $db -> prepare($insert_query);
         $statement -> bindValue(':card_id', $card_id);
         $statement -> bindValue(':mana_id', $mana_id);
         $statement -> execute();
+    }
+}
+
+// Saves deck's cards into deck_cards table.
+function save_deck_cards($db, $deck_id, $decklist) {
+    foreach ($decklist as $card_name => $quantity) {
+        // Find the card ID. If it doesn't exist, exist it.
+        $card_id = find_card($db, $card_name);
+
+        $query = "INSERT INTO deck_cards (deck_id, card_id, quantity) 
+        VALUES (:deck_id, :card_id, :quantity)";
+
+        $statement = $db -> prepare($query);
+        $statement -> bindValue(':deck_id', $deck_id);
+        $statement -> bindValue(':card_id', $card_id);
+        $statement -> bindValue(':quantity', $quantity);
+        $statement -> execute();
+    }
+}
+
+function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+    $current_folder = dirname(__FILE__);
+    
+    // Build an array of paths segment names to be joins using OS specific slashes.
+    $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+    
+    // The DIRECTORY_SEPARATOR constant is OS agnostic.
+    return join(DIRECTORY_SEPARATOR, $path_segments);
+}
+
+ // Checks if the file is a valid image or PDF.
+function file_is_valid($temporary_path, $new_path) {
+    $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png'];
+    $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png'];
+     
+    $actual_file_extension   = pathinfo($new_path, PATHINFO_EXTENSION);
+    $actual_mime_type        = mime_content_type($temporary_path);
+     
+    $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+    $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
+     
+    return $file_extension_is_valid && $mime_type_is_valid;
+}
+
+function upload_image() {
+    $image_filename        = $_FILES['image']['name'];
+    $temporary_image_path  = $_FILES['image']['tmp_name'];
+    $new_image_path        = file_upload_path($image_filename);
+
+    if (file_is_valid($temporary_image_path, $new_image_path)) {
+        move_uploaded_file($temporary_image_path, $new_image_path);
+        return $new_image_path;
     }
 }
 
@@ -206,21 +262,29 @@ if ($_POST) {
     $archetype = filter_input(INPUT_POST, 'archetype', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $decklist_raw = filter_input(INPUT_POST, 'decklist', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    // TODO: Add better error processing.
+    $image_upload_detected = isset($_FILES['image']) && ($_FILES['image']['error'] === 0);
+    $upload_error_detected = isset($_FILES['image']) && ($_FILES['image']['error'] > 0);
+
+    // TODO: Add better error processing for the below checks.
     if (!in_array($archetype, $archetypes)) {
         header("Location: error.php");
         exit;
     }
 
-    // TODO: Add better error processing.
     // If title or content has less than 1 character, display error page.
     if (strlen($title) < 1 || strlen($commander_name) < 1) {
         header("Location: error.php");
         exit;
     }
 
-    // TODO: Add better error processing.
     if (empty($decklist_raw)) {
+        header("Location: error.php");
+        exit;
+    }
+
+    if ($image_upload_detected && !$upload_error_detected) {
+        $image_path = upload_image();
+    } else {
         header("Location: error.php");
         exit;
     }
@@ -232,10 +296,10 @@ if ($_POST) {
     $decklist = parse_decklist($decklist_raw);
 
     // Save deck to decks table.
-    $deck_id = save_deck($db, $title, $description, $commander_id, $archetype, $user_id);
+    $deck_id = save_deck($db, $title, $description, $commander_id, $image_path, $archetype, $user_id);
 
     // Save deck list to deck_cards table.
-    // save_deck_cards($db, $deck_id, $decklist);
+    save_deck_cards($db, $deck_id, $decklist);
 }
 ?>
 
