c46de97188b5cc2ec11aba89eea01962ff40ea53
Ian-Protocol <94944192+Ian-Protocol@users.noreply.github.com>
2025-03-30 20:24:58 -0500

    Add image resizing

--- create_deck.php
+++ create_deck.php
@@ -6,10 +6,14 @@ require './includes/authenticate.php';
 // Connects to the database.
 require './includes/connect.php';
 
-// Image resizing if necessary.
-// require 'C:\xampp\htdocs\_Assignments\php-image-resize-master\lib\ImageResize.php';
+// Image resizing
+require 'C:\xampp\htdocs\_Assignments\php-image-resize-master\lib\ImageResize.php';
 // require 'C:\xampp\htdocs\_Assignments\php-image-resize-master\lib\ImageResizeException.php';
-// use \Gumlet\ImageResize;
+use \Gumlet\ImageResize;
+
+const REGULAR_WIDTH = 400;
+const THUMBAIL_WIDTH = 50;
+const UPLOAD_SUBFOLDER_NAME = 'uploads';
 
 $page_title = "Commander Deckbuilder - Create Deck";
 
@@ -238,11 +242,11 @@ function save_deck_cards($db, $deck_id, $decklist) {
     }
 }
 
-function file_upload_path($original_filename, $upload_subfolder_name = 'uploads') {
+function file_upload_path($original_filename) {
     $current_folder = dirname(__FILE__);
     
     // Build an array of paths segment names to be joins using OS specific slashes.
-    $path_segments = [$current_folder, $upload_subfolder_name, basename($original_filename)];
+    $path_segments = [$current_folder, UPLOAD_SUBFOLDER_NAME, basename($original_filename)];
     
     // The DIRECTORY_SEPARATOR constant is OS agnostic.
     return join(DIRECTORY_SEPARATOR, $path_segments);
@@ -268,15 +272,42 @@ function upload_image($db, $deck_id) {
     $new_image_path        = file_upload_path($image_filename);
 
     if (file_is_valid($temporary_image_path, $new_image_path)) {
-        move_uploaded_file($temporary_image_path, $new_image_path);
+        $image = new ImageResize($temporary_image_path);
+
+            $regular_filename = substr_replace($image_filename, "_regular",
+                strrpos($image_filename, ".")) . "." . pathinfo($image_filename, PATHINFO_EXTENSION);
+
+            $thumbnail_filename = substr_replace($image_filename, "_thumbnail",
+            strrpos($image_filename, ".")) . "." . pathinfo($image_filename, PATHINFO_EXTENSION);
+
+            // Resize medium path
+            $regular_path = substr_replace($new_image_path, $regular_filename, 
+            strrpos($new_image_path, DIRECTORY_SEPARATOR) + 1);
+            
+            // Resize thumbnail path
+            $thumbnail_path = substr_replace($new_image_path, $thumbnail_filename, 
+            strrpos($new_image_path, DIRECTORY_SEPARATOR) + 1);
+
+            // Resize and save image.
+            $image
+                ->resizeToWidth(REGULAR_WIDTH)
+                ->save($regular_path)
+
+                ->resizeToWidth(THUMBAIL_WIDTH)
+                ->save($thumbnail_path)
+            ;
     }
 
-    $query = "INSERT INTO images (deck_id, image_url) 
-    VALUES (:deck_id, :image_url)";
+    $query = "INSERT INTO images (deck_id, regular_path, thumbnail_path) 
+    VALUES (:deck_id, :regular_path, :thubmnail_path)";
+
+    $regular_filename = "." . DIRECTORY_SEPARATOR . UPLOAD_SUBFOLDER_NAME . $regular_filename;
+    $thumbnail_filename = "." . DIRECTORY_SEPARATOR . UPLOAD_SUBFOLDER_NAME . $thumbnail_filename;
 
     $statement = $db -> prepare($query);
     $statement -> bindValue(':deck_id', $deck_id);
-    $statement -> bindValue(':image_url', $new_image_path);
+    $statement -> bindValue(':regular_path', $regular_filename);
+    $statement -> bindValue(':thumbnail_path', $thumbnail_filename);
     $statement -> execute();
 }
 
