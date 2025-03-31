4cea39cf3d40a751a0f6a81f3e45a9ba2485c62d
Ian-Protocol <94944192+Ian-Protocol@users.noreply.github.com>
2025-03-30 23:27:20 -0500

    Add register.php for user signups

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+$page_title = "Commander Deckbuilder - Register";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include './includes/head.php'; ?>
+    <body>
+        <h1>Commander Deckbuilder - Resigster</h1>
+        <?php include './includes/navbar.php'; ?>
+
+        <div id="register">
+            <h2>Sign up to start building and enjoy the community!</h2>
+        </div>
+    </body>
+    <?php include './includes/footer.php'; ?>
+</html>
