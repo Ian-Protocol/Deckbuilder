3d5f44cdee44122b571fbce99b66f2435d1787e9
Ian-Protocol <94944192+Ian-Protocol@users.noreply.github.com>
2025-03-31 19:04:49 -0500

    Add login functionality

--- login.php
+++ login.php
@@ -2,6 +2,39 @@
 require 'includes/connect.php';
 
 $page_title = "Commander Deckbuilder - Log In";
+$messages = [];
+$login_successful = false;
+
+if ($_POST) {
+    // Should I sanitize this value?
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = :username";
+    $statement = $db -> prepare($query);
+    $statement -> bindValue('username', $username);
+    $statement -> execute();
+
+    $user = $statement -> fetch() ?? null;
+
+    if ($user) {
+        $password_hash = $user['password'];
+
+        if (password_verify($password, $password_hash)) {
+            $messages[] = "Login successful!";
+            $login_successful = true;
+
+            // Set session values.
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['role'] = $user['role'];
+            $_SESSION['username'] = $user['username'];
+        } else {
+            $messages[] = "Incorrect password. Please try again.";
+        }
+    } else {
+        $messages[] = "User not found. Please try again.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +46,24 @@ $page_title = "Commander Deckbuilder - Log In";
 
         <div id="register">
             <h2>Log in to start building and enjoy the community!</h2>
+            <form action="login.php" method="post">
+                <label for="username">Username:</label><input type="text" id="username" name="username" required />
+                <label for="password">Password:</label><input type="password" id="password" name="password" required />
+                <input type="submit" value="Log In">
+            </form>
         </div>
+            <div id="message">
+                <?php if ($login_successful): ?>
+                    <?php foreach ($messages as $message): ?>
+                        <p><?= $message ?></p>
+                    <?php endforeach ?>
+                    <a href="index.php">Go to homepage</a>
+                <?php else: ?>
+                    <?php foreach ($messages as $message): ?>
+                        <p><?= $message ?></p>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </div>
     </body>
     <?php include './includes/footer.php'; ?>
 </html>
